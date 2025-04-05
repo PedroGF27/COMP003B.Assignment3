209ee9185aca1df829b5ebcb240ca44a98fcd169
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-04-04 19:19:26 -0700

    Added the eventcontroller.cs with the necessary code

---
+++ COMP003B.Assignment3/Controllers/EventController.cs
@@ -0,0 +1,43 @@
+﻿using COMP003B.Assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment3.Controllers
+{
+	public class EventController : Controller
+	{
+		// Gets event code
+		[HttpGet("event/register/{eventcode}")]
+
+		public IActionResult Register(string eventcode)
+		{
+			var model = new EventRegistration();
+
+			return View(model);
+		}
+
+		[HttpGet("event/register")]
+
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost("event/register")]
+
+		public IActionResult Register([FromBody] EventRegistration model)
+		{
+			if (ModelState.IsValid)
+			{
+				return View("Register", model);
+				
+			}
+			return RedirectToAction("Successful");
+		}
+
+		[HttpGet("success")]
+		public IActionResult Success(EventRegistration view)
+		{
+			return View(view);
+		}
+	}
+}
